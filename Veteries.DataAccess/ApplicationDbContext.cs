7948b0571da7db1ebe84d200b23eb4cc943bc625
Eklerek <mwiczuk99@gmail.com>
2020-01-16 00:13:09 +0100

    Add ApplicationDbContext class

---
+++ Veteries.DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Veteries.Models;
+
+namespace Veteries.DataAccess
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Species> Species { get; set; }
+    }
+}
