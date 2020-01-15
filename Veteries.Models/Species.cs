a4ee12c0c65b49ab8a9de3a0f9da7e476d3978b3
Eklerek <mwiczuk99@gmail.com>
2020-01-16 00:12:44 +0100

    Add species model

---
+++ Veteries.Models/Species.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Veteries.Models
+{
+    public class Species
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
