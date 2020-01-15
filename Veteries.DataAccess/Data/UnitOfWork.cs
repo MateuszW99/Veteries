1f5cf3b61ffc275281e7834a236729ad53031595
Eklerek <mwiczuk99@gmail.com>
2020-01-16 00:13:47 +0100

    i Implement IUnitOfWork interface

---
+++ Veteries.DataAccess/Data/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Veteries.DataAccess.Data.Repository;
+using Veteries.DataAccess.Data.Repository.IRepository;
+
+namespace Veteries.DataAccess.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Species = new SpeciesRepository(_db);
+        }
+
+        public ISpeciesRepository Species { get; private set; }
+
+        public ISpeciesRepository SpeciesRepository => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
