ce2fd90c1f98cd00a8ed3ad4a93800a40c728079
Eklerek <mwiczuk99@gmail.com>
2020-01-16 23:44:04 +0100

    Fix a bug where edit and delete buttons were covering each other

--- Veteries/wwwroot/js/species.js
+++ Veteries/wwwroot/js/species.js
@@ -16,7 +16,7 @@ function loadList() {
             {
                 "data": "id",
                 "render": function (data) {
-                    return ` <d<a href="/Admin/species/upsert?id=${data}" class="btn btn-success text-white" style="cursor:pointer; width:100px;">
+                    return ` <a href="/Admin/species/upsert?id=${data}" class="btn btn-success text-white" style="cursor:pointer; width:100px;">
                                     <i class="far fa-edit"></i> Edit
                                 </a>
                                 <a class="btn btn-danger text-white" style="cursor:pointer; width:100px;" onclick=Delete('/api/category/'+${data})>
