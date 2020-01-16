01e27a88632ffb5eddc5535cc7cffb8a99d43bac
Eklerek <mwiczuk99@gmail.com>
2020-01-16 23:14:26 +0100

    Add Species API controller

---
+++ Veteries/Controllers/SpeciesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Veteries.DataAccess.Data.Repository.IRepository;
+
+namespace Veteries.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SpeciesController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public SpeciesController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Json(new { data = _unitOfWork.Species.GetAll() });
+        }
+    }
+}
