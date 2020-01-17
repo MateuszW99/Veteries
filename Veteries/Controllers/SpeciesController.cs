344fca82fba648357b1bdebd442d00e89eee32d6
Eklerek <mwiczuk99@gmail.com>
2020-01-17 22:22:47 +0100

    Add Delete method to SpeciesController

--- Veteries/Controllers/SpeciesController.cs
+++ Veteries/Controllers/SpeciesController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Veteries.DataAccess.Data.Repository.IRepository;
 
 namespace Veteries.Controllers
@@ -23,6 +18,19 @@ namespace Veteries.Controllers
         public IActionResult Get()
         {
             return Json(new { data = _unitOfWork.Species.GetAll() });
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.Species.GetFirstOrDefault(u => u.Id == id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting." });
+            }
+            _unitOfWork.Species.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Deleting complete." });
         }
     }
 }
