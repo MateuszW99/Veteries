5bc02ad4ea95db4dbc6aeeec94f9b2d29caf4ea5
Eklerek <mwiczuk99@gmail.com>
2020-01-17 22:39:22 +0100

    Add js script to delete Species from database

--- Veteries/wwwroot/js/species.js
+++ Veteries/wwwroot/js/species.js
@@ -19,7 +19,7 @@ function loadList() {
                     return ` <a href="/Admin/species/upsert?id=${data}" class="btn btn-success text-white" style="cursor:pointer; width:100px;">
                                     <i class="far fa-edit"></i> Edit
                                 </a>
-                                <a class="btn btn-danger text-white" style="cursor:pointer; width:100px;" onclick=Delete('/api/category/'+${data})>
+                                <a class="btn btn-danger text-white" style="cursor:pointer; width:100px;" onclick=Delete('/api/species/'+${data})>
                                     <i class="far fa-trash-alt"></i> Delete
                                 </a>
                             </div>`;
@@ -30,5 +30,32 @@ function loadList() {
             "emptyTable": "No data found."
         },
         "width": "100%"
+    });
+}
+
+
+function Delete(url) {
+    swal({
+        title: "Are you sure you want to Delete?",
+        text: "You will not be able to restore the data!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: 'DELETE',
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
     });
 }
