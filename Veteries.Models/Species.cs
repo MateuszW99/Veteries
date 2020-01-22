d0dff6c6599f3e60096b3375dfd69f980efab192
Eklerek <mwiczuk99@gmail.com>
2020-01-22 21:19:33 +0100

    Add regex and error message for Name

--- Veteries.Models/Species.cs
+++ Veteries.Models/Species.cs
@@ -8,6 +8,8 @@ namespace Veteries.Models
         public int Id { get; set; }
 
         [Required]
+        [StringLength(20, MinimumLength = 3)]
+        [RegularExpression(@"[A-Z][a-z]+", ErrorMessage = "The Name should start with an upper-case letter and mustn't contain anything besides letters")]
         public string Name { get; set; }
     }
 }
