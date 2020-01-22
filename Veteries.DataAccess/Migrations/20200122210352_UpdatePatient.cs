615c0a26284c7d4bae70bcdfe50171a0fe0c91c6
Eklerek <mwiczuk99@gmail.com>
2020-01-22 22:07:53 +0100

    Db update for updated PatientRepository

---
+++ Veteries.DataAccess/Migrations/20200122210352_UpdatePatient.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Veteries.DataAccess.Migrations
+{
+    public partial class UpdatePatient : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EailAddress",
+                table: "Person");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Species",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmailAddress",
+                table: "Person",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Age",
+                table: "Patient",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailAddress",
+                table: "Person");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Species",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 20);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EailAddress",
+                table: "Person",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Age",
+                table: "Patient",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int));
+        }
+    }
+}
