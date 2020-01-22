63e8bb6315255c53e09f88452907db48074938cb
Eklerek <mwiczuk99@gmail.com>
2020-01-22 22:23:06 +0100

    Add Get() method

--- Veteries/Controllers/PatientController.cs
+++ Veteries/Controllers/PatientController.cs
@@ -2,14 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web.Helpers;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Veteries.DataAccess.Data.Repository.IRepository;
 
 namespace Veteries.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class PatientController : ControllerBase
+    public class PatientController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public PatientController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Json(new { data = _unitOfWork.Patient.GetAll() });
+        }
+
     }
 }
