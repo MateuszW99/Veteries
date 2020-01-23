9a2c399c8b70f1e7ee14f87844f0f984b987f24c
Eklerek <mwiczuk99@gmail.com>
2020-01-23 11:31:32 +0100

    Add OnGet() method

--- Veteries/Pages/Admin/Patients/Upsert.cshtml.cs
+++ Veteries/Pages/Admin/Patients/Upsert.cshtml.cs
@@ -4,14 +4,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Veteries.DataAccess.Data.Repository.IRepository;
+using Veteries.Models;
 
 namespace Veteries.Pages.Admin.Patients
 {
     public class UpsertModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly IUnitOfWork _unitOfWork;
 
+        public UpsertModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [BindProperty]
+        public Patient PatientObj { get; set; }
+
+        public IActionResult OnGet(int? id)
+        {
+            PatientObj = new Patient();
+            if (id != null)
+            {
+                PatientObj = _unitOfWork.Patient.GetFirstOrDefault(s => s.Id == id);
+                if (PatientObj == null)
+                {
+                    return NotFound();
+                }
+            }
+            return Page();
         }
     }
 }
