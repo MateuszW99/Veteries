9a024c0f842f968f2948fcac7f2e7561c2d7a3e2
Eklerek <mwiczuk99@gmail.com>
2020-02-09 13:04:16 +0100

    Add Patient view model

---
+++ Veteries.Models/ViewModels/PatientVM.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace Veteries.Models.ViewModels
+{
+    public class PatientVM
+    {
+        public Patient Patient { get; set; }
+        public IEnumerable<SelectListItem> SpeciesList { get; set; }
+    }
+}
