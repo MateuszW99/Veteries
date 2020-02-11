e2661792ea5052d14c7efcca69472fcc94e6e2a3
Eklerek <mwiczuk99@gmail.com>
2020-02-11 23:40:11 +0100

    Add Veterinarian class model

---
+++ Veteries.Models/Interfaces/IVeterinarian.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Veteries.Models.Interfaces
+{
+    public interface IVeterinarian
+    {
+        public string OfficeName { get; set; }
+        public string Address { get; set; }
+        //public string WorkingHours { get; set; }
+        // TODO: add Schedule prop
+        public string PaymentRange { get; set; }
+    }
+}
---
+++ Veteries.Models/Veterinarian.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Veteries.Models.Interfaces;
+
+namespace Veteries.Models
+{
+    public class Veterinarian : IVeterinarian, IPerson
+    {
+        [Required]
+        [StringLength(15, MinimumLength = 2)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(15, MinimumLength = 2)]
+        public string LastName { get; set; }
+
+        [Required]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        public string OfficeName { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        [RegularExpression(@"\d{2,4}-\d{2,4}")]
+        public string PaymentRange { get; set; }
+    }
+}
