da90f352abc3def7bbd1c430f53878c17580ce09
Eklerek <mwiczuk99@gmail.com>
2020-02-11 23:42:56 +0100

    Add Customer controller

---
+++ Veteries/Controllers/CustomerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Veteries.DataAccess.Data;
+
+namespace Veteries.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : Controller
+    {
+        private readonly UnitOfWork _unitOfWork;
+        public CustomerController(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+    }
+}
