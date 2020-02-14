29ac31278661369c88d5a4de9a3f88eaa8cdca96
Eklerek <mwiczuk99@gmail.com>
2020-02-14 17:52:07 +0100

    Add Address class

---
+++ Veteries.Models/Address.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Veteries.Utility.UtilityModels.Interfaces;
+
+namespace Veteries.Models
+{
+    public class Address : IAddress
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [RegularExpression(@"[A-Z][a-z]+")]
+        public string City { get; set; }
+
+        [Required]
+        public string Street { get; set; }
+
+        [Required, Display(Name = "Zip code")]
+        [RegularExpression(@"\d{2}-?\d{3,4}")]
+        public string ZipCode { get; set; }
+
+        [Display(Name = "Veterinarian")]
+        public int VetId { get; set; }
+
+        [ForeignKey("VetId")]
+        public virtual Veterinarian Veterinarian { get; set; }
+    }
+}
---
+++ Veteries.Models/Interfaces/IAddress.cs
@@ -0,0 +1,9 @@
+﻿namespace Veteries.Utility.UtilityModels.Interfaces
+{
+    public interface IAddress
+    {
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
