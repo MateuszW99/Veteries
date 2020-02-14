0d763d734f22f2c4de14f00d7fb7534b4f10a275
Eklerek <mwiczuk99@gmail.com>
2020-02-14 19:08:52 +0100

    Add migration with Veterinarian and Address models

--- Veteries.DataAccess/Migrations/20200214180018_AddVeterinarianAndAddressMigration.Designer.cs
+++ Veteries.DataAccess/Migrations/20200214180018_AddVeterinarianAndAddressMigration.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Veteries.DataAccess;
 
 namespace Veteries.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200214180018_AddVeterinarianAndAddressMigration")]
+    partial class AddVeterinarianAndAddressMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
