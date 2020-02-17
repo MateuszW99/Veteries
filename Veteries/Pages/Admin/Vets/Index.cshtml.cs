47f7d1330ce6e97201a0730e7fbf53b105d0a30b
Eklerek <mwiczuk99@gmail.com>
2020-02-17 22:04:27 +0100

    Add Index page for Vets

---
+++ Veteries/Pages/Admin/Vets/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Veteries.DataAccess.Data.Repository.IRepository;
+using Veteries.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace Veteries.Pages.Admin.Vets
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public List<Veterinarian> Veterinarians { get; set; }
+
+        public IndexModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+
+        public void OnGet()
+        {
+            Veterinarians = (List<Veterinarian>)_unitOfWork.Veterinarian.GetAll();
+
+        }
+    }
+}
