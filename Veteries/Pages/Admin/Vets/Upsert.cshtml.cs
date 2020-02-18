39f7421310aedb75b339940ea5ebe1e48603291c
Eklerek <mwiczuk99@gmail.com>
2020-02-18 19:28:25 +0100

    1. Fixed a bug where data woudln't be inserted to db 2. Data is displayed in 1 row instead of 1 column

--- Veteries/Pages/Admin/Vets/Upsert.cshtml.cs
+++ Veteries/Pages/Admin/Vets/Upsert.cshtml.cs
@@ -20,11 +20,15 @@ namespace Veteries.Pages.Admin.Vets
         [BindProperty]
         public Models.Veterinarian VeterinarianObj { get; set; }
 
+        [BindProperty]
+        public Models.Address AddressObj { get; set; }
+
         public IActionResult OnGet(int? id)
         {
             VeterinarianObj = new Models.Veterinarian();
             if (id != null)
             {
+                AddressObj = _unitOfWork.Address.GetFirstOrDefault(u => u.Id == id);
                 VeterinarianObj = _unitOfWork.Veterinarian.GetFirstOrDefault(u => u.Id == id);
             }
             if (VeterinarianObj == null)
@@ -38,14 +42,22 @@ namespace Veteries.Pages.Admin.Vets
         {
             if (!ModelState.IsValid)
             {
+                string messages = String.Join(Environment.NewLine, ModelState.Values.SelectMany(v => v.Errors)
+                                                           .Select(v => v.ErrorMessage + " " + v.Exception));
                 return Page();
             }
-            if (VeterinarianObj.Id == 0)
+            if (VeterinarianObj.Id == 0 && AddressObj.Id == 0)
             {
+                _unitOfWork.Address.Add(AddressObj);
+                _unitOfWork.Save();
+                VeterinarianObj.AddressId = AddressObj.Id;
                 _unitOfWork.Veterinarian.Add(VeterinarianObj);
             }
             else
             {
+                _unitOfWork.Address.Update(AddressObj);
+                _unitOfWork.Save();
+                VeterinarianObj.AddressId = AddressObj.Id;
                 _unitOfWork.Veterinarian.Update(VeterinarianObj);
             }
             _unitOfWork.Save();
