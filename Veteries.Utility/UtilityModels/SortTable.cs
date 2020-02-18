3fe4b3f9067e00fa42e0d2e31f4e4e02f709fd98
Eklerek <mwiczuk99@gmail.com>
2020-02-18 22:58:48 +0100

    Add SortTable as a class helper 1. Added the class SortTable, which sorts vetlist by given parameter 2. Added dependency for Veteries.Models

---
+++ Veteries.Utility/UtilityModels/SortTable.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Veteries.Models;
+using Veteries.Utility.UtilityModels.Interfaces;
+
+namespace Veteries.Utility.UtilityModels
+{
+    public static class SortTable
+    {
+        public static List<Veterinarian> SortVets(string sortOrder, List<Veterinarian> veterinarians)
+        {
+            switch (sortOrder)
+            {
+                case "office_desc":
+                    {
+                        veterinarians = veterinarians.OrderByDescending(s => s.OfficeName).ToList();
+                        break;
+                    }
+
+                case "FNameSort":
+                    {
+                        veterinarians = veterinarians.OrderBy(s => s.FirstName).ToList();
+                        break;
+                    }
+
+                case "fName_desc":
+                    {
+                        veterinarians = veterinarians.OrderByDescending(s => s.FirstName).ToList();
+                        break;
+                    }
+
+                case "LNameSort":
+                    {
+                        veterinarians = veterinarians.OrderBy(s => s.LastName).ToList();
+                        break;
+                    }
+
+                case "lName_desc":
+                    {
+                        veterinarians = veterinarians.OrderByDescending(s => s.LastName).ToList();
+                        break;
+                    }
+
+                case "EmailSort":
+                    {
+                        veterinarians = veterinarians.OrderBy(s => s.EmailAddress).ToList();
+                        break;
+                    }
+
+                case "email_desc":
+                    {
+                        veterinarians = veterinarians.OrderByDescending(s => s.EmailAddress).ToList();
+                        break;
+                    }
+
+                case "PhoneSort":
+                    {
+                        veterinarians = veterinarians.OrderBy(s => s.PhoneNumber).ToList();
+                        break;
+                    }
+
+                case "phone_desc":
+                    {
+                        veterinarians = veterinarians.OrderByDescending(s => s.PhoneNumber).ToList();
+                        break;
+                    }
+            }
+            return veterinarians.ToList();
+        }
+    }
+}
