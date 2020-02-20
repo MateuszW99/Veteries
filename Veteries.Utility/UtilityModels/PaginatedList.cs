de574cb780a96c99376bd078a2827ba89462d2b7
Eklerek <mwiczuk99@gmail.com>
2020-02-20 23:54:47 +0100

    Add extension method to paginate given list

---
+++ Veteries.Utility/UtilityModels/PaginatedList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Veteries.Utility.UtilityModels
+{
+    public static class PaginatedList
+    {
+       public static List<T> CreatePagination<T>(this List<T> source, int pageIndex, int pageSize)
+       {
+            return source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
+       }
+    }
+}
