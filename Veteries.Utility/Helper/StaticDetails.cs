e9561e8d75977ba4266d869a0aee736677d8ac2e
Eklerek <mwiczuk99@gmail.com>
2020-02-23 20:38:22 +0100

    Add file with const variables

---
+++ Veteries.Utility/Helper/StaticDetails.cs
@@ -0,0 +1,11 @@
+﻿namespace Veteries.Utility.Helper
+{
+    public class StaticDetails
+    {
+        public const int MaxPageSize = 10;
+
+        public const string AdminRole = "Admin";
+        public const string OfficeOwnerRole = "OfficeOwner";
+        public const string CustomerRole = "Customer";
+    }
+}
