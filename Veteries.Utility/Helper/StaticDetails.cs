dfed1edeb565e4532038fca58dab5f7fb721f1d0
Eklerek <mwiczuk99@gmail.com>
2020-02-25 23:06:35 +0100

    Add radio buttons with role and register button

--- Veteries.Utility/Helper/StaticDetails.cs
+++ Veteries.Utility/Helper/StaticDetails.cs
@@ -1,11 +1,21 @@
-﻿namespace Veteries.Utility.Helper
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Veteries.Utility.Helper
 {
     public class StaticDetails
     {
         public const int MaxPageSize = 10;
 
+        [Display(Name = "Admin")]
         public const string AdminRole = "Admin";
+
+        [Display(Name = "Maintenance")]
+        public const string MaintenanceRole = "Maintenance";
+
+        [Display(Name = "Office Owner")]
         public const string OfficeOwnerRole = "OfficeOwner";
+
+
         public const string CustomerRole = "Customer";
     }
 }
