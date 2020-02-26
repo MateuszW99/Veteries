7e52a303e428a3a1b1bc1c1d5bb6a672570d6b3a
Eklerek <mwiczuk99@gmail.com>
2020-02-26 19:38:15 +0100

    1. Add users page 2. Add script to get users into table

---
+++ Veteries/Pages/Admin/Users/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Veteries.Pages.Admin.Users
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
