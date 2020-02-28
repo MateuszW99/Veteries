d73150ce92f2db1cf401afe33fbb19e21aa44daa
Eklerek <mwiczuk99@gmail.com>
2020-02-28 21:42:31 +0100

    1. Button to lock/unlock changes color depending on previous function called 2. Swapped the buttons' colors

--- Veteries/wwwroot/js/users.js
+++ Veteries/wwwroot/js/users.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function () {
     loadList();
-}); 
+});
 
 function loadList() {
     dataTable = $('#DT_users').DataTable({
@@ -16,27 +16,27 @@ function loadList() {
             { "data": "email", "width": "25%" },
             { "data": "phoneNumber", "width": "25%" },
             {
-                "data": {id: "id", lockoutEnd: "lockoutEnd"},
+                "data": { id: "id", lockoutEnd: "lockoutEnd" },
                 "render": function (data) {
-                    var today = new Date;
-                    today = today.getDate();
-                    var lockout = new Date(data.lockoutEnd);
-                    lockout = lockout.getDate();
+                    var today = new Date().getTime();
+                    var lockout = new Date(data.lockoutEnd).getTime();
                     if (lockout > today) {
-                        // user is locked
-                        return `<div class="text-center">
-                                    <a class="btn btn-danger text-white" style="cursor:pointer; width:100px;" onclick=LockUnlock('${data.id}')>
-                                        <i class="fas fa-lock-open"></i> Unlock
+                        // Current user is locked
+                        return ` <div class="text-center">
+                                    <a class="btn btn-success text-white" style="cursor:pointer; width:100px;" onclick=LockUnlock('${data.id}')>
+                                       <i class="fas fa-lock-open"></i> Unlock
                                     </a>
                                 </div>`;
+
                     }
                     else {
-                        return `<div class="text-center">
-                                    <a class="btn btn-success text-white" style="cursor:pointer; width:100px;" onclick=LockUnlock('${data.id}')>
+                        return ` <div class="text-center">
+                                    <a class="btn btn-danger text-white" style="cursor:pointer; width:100px;" onclick=LockUnlock('${data.id}')>
                                         <i class="fas fa-lock"></i> Lock
                                     </a>
                                 </div>`;
                     }
+
                 }, "width": "30%"
             }
         ],
@@ -50,13 +50,13 @@ function loadList() {
 function LockUnlock(id) {
     $.ajax({
         type: 'POST',
-        url: 'api/user',
+        url: '/api/user',
         data: JSON.stringify(id),
-        contentType: 'application/json',
+        contentType: "application/json",
         success: function (data) {
             if (data.success) {
                 toastr.success(data.message);
-                dataTable.reload();
+                dataTable.ajax.reload();
             }
             else {
                 toastr.error(data.message);
