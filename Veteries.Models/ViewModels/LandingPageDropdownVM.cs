88f2be630ee83e67c870789188bd3672092dabe8
Eklerek <mwiczuk99@gmail.com>
2020-03-04 20:33:28 +0100

    Add model for thr landing page

---
+++ Veteries.Models/ViewModels/LandingPageDropdownVM.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace Veteries.Models.ViewModels
+{
+    public class LandingPageDropdownVM
+    {
+        public int CityID { get; set; }
+        public IEnumerable<SelectListItem> CityList { get; set; }
+
+        public int SpeciesID { get; set; }
+        public IEnumerable<SelectListItem> SpeciesList { get; set; }
+
+    }
+}
