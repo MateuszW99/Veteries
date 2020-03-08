0bebe19e2b1d4be273bed1e9f12beafb155977c0
Eklerek <mwiczuk99@gmail.com>
2020-03-08 11:15:35 +0100

    Add VM containing refernces to city and species

---
+++ Veteries.Models/ViewModels/MainSearchVM.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Veteries.Models.ViewModels
+{
+    public class MainSearchVM
+    {
+        [Required]
+        public int CityID { get; set; }
+
+        [Required]
+        public int SpeciesID { get; set; }
+    }
+}
