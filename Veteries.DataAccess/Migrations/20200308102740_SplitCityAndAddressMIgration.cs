33358ac8ca8ee9159c850297358435b795bda677
Eklerek <mwiczuk99@gmail.com>
2020-03-08 11:30:42 +0100

    Add City Migration to DB

---
+++ Veteries.DataAccess/Migrations/20200308102740_SplitCityAndAddressMIgration.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Veteries.DataAccess.Migrations
+{
+    public partial class SplitCityAndAddressMIgration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Address");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CityID",
+                table: "Address",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "City",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_City", x => x.ID);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Address_CityID",
+                table: "Address",
+                column: "CityID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Address_City_CityID",
+                table: "Address",
+                column: "CityID",
+                principalTable: "City",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Address_City_CityID",
+                table: "Address");
+
+            migrationBuilder.DropTable(
+                name: "City");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Address_CityID",
+                table: "Address");
+
+            migrationBuilder.DropColumn(
+                name: "CityID",
+                table: "Address");
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Address",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
