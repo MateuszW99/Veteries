cf3e1d40124b12b19b9ca00e21423ee9b362eaee
Eklerek <mwiczuk99@gmail.com>
2020-03-08 11:30:00 +0100

    Minor changes

--- Veteries.DataAccess/Data/Repository/IRepository/ICityRepository.cs
+++ Veteries.DataAccess/Data/Repository/IRepository/ICityRepository.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
-using System;
 using System.Collections.Generic;
-using System.Text;
 using Veteries.Models;
 
 namespace Veteries.DataAccess.Data.Repository.IRepository
