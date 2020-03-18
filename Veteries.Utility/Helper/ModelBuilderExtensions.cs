e94663809def84e25fc43bf872da431d6dd906ed
Eklerek <mwiczuk99@gmail.com>
2020-03-18 22:28:14 +0100

    Seed Cities table 1. Override OnModelCreating() method 2. Create extension method for ModelBuilder

---
+++ Veteries.Utility/Helper/ModelBuilderExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Veteries.Models;
+
+namespace Veteries.Utility.Helper
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void SeedCities(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<City>().HasData(
+                new City
+                {
+                    ID = 1010,
+                    Name = "Warszawa"
+                },
+                new City
+                {
+                    ID = 1011,
+                    Name = "Kraków"
+                },
+                new City
+                {
+                    ID = 1012,
+                    Name = "Lublin"
+                },
+                new City
+                {
+                    ID = 1013,
+                    Name = "Poznań"
+                });
+        }
+    }
+    
+}
