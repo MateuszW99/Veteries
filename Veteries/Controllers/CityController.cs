aedb056183d69994c140d4c59ae17f8d3ea50d7b
Eklerek <mwiczuk99@gmail.com>
2020-03-20 21:51:12 +0100

    Add City pages

--- Veteries/Controllers/CityController.cs
+++ Veteries/Controllers/CityController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Veteries.DataAccess.Data.Repository.IRepository;
 
 namespace Veteries.Controllers
@@ -25,6 +20,19 @@ namespace Veteries.Controllers
         {
             return Json(new { data = _unitOfWork.City.GetAll() });
         }
-        
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.City.GetFirstOrDefault(u => u.ID == id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting." });
+            }
+            _unitOfWork.City.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, messsage = "Deleted successfully." });
+        }
+
     }
 }
---
+++ Veteries/Pages/Admin/Cities/Upsert.cshtml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Veteries.DataAccess.Data.Repository.IRepository;
+using Veteries.Models;
+
+namespace Veteries.Pages.Admin.Cities
+{
+    public class UpsertModel : PageModel
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UpsertModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [BindProperty]
+        public City CityObj { get; set; }
+
+        public IActionResult OnGet(int? id)
+        {
+            CityObj = new City();
+            if (id != null)
+            {
+                CityObj = _unitOfWork.City.GetFirstOrDefault(s => s.ID == id);
+                if (CityObj == null)
+                {
+                    return NotFound();
+                }
+            }
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            if (CityObj.ID == 0)
+            {
+                _unitOfWork.City.Add(CityObj);
+            }
+            else
+            {
+                _unitOfWork.City.Update(CityObj);
+            }
+            _unitOfWork.Save();
+            return RedirectToPage("./Index");
+        }
+    }
+}
